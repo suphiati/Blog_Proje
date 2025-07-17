364d7508297fea289b60f240dc51d409bd06ecfc
suphiati <suphi.celikoz@gmail.com>
2025-07-17 22:18:35 +0300

    17/07/25

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const slides = document.querySelectorAll(".slide");
+  let currentIndex = 0;
+
+  setInterval(() => {
+    slides[currentIndex].classList.remove("active");
+    currentIndex = (currentIndex + 1) % slides.length;
+    slides[currentIndex].classList.add("active");
+  }, 3000);
+});
